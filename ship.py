a891891804900f87a0bee5d08a7d6c6a02cce8ec
belieffsy <38274401+belieffsy@users.noreply.github.com>
2018-11-21 21:39:52 +0800

    Update ship.py

--- ship.py
+++ ship.py
@@ -1,25 +1,26 @@
 import pygame
 
+
 class Ship():
-    def __init__(self,ai_settings,screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船设置并设置其初始位置"""
         self.screen = screen
 
-        #加载飞船图像，并获取其外接矩形
+        # 加载飞船图像，并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
-        self.rect=self.image.get_rect()
-        self.screen_rect=screen.get_rect()
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
 
-        #将每艘新飞船放在屏幕底部中央
-        self.rect.centerx=self.screen_rect.centerx
+        # 将每艘新飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        #在飞船的属性center中存储小数值
+        # 在飞船的属性center中存储小数值
         self.center = float(self.rect.centerx)
         self.centery = float(self.rect.centery)
 
-        #移动标志
+        # 移动标志
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
@@ -27,24 +28,21 @@ class Ship():
 
     def update(self):
         """根据移动标志调整飞船的位置"""
-        #更新飞船的center值，而不知rect
-        if self.moving_right and self.rect.right< self.screen_rect.right:
+        # 更新飞船的center值，而不知rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left and self.rect.left>0:
+        if self.moving_left and self.rect.left > 0:
             self.center -= self.ai_settings.ship_speed_factor
-        if self.moving_up and self.rect.top>0:
-            self.centery -=self.ai_settings.ship_speed_factor
-        if self.moving_down and self.rect.bottom<self.screen_rect.bottom:
+        if self.moving_up and self.rect.top > 0:
+            self.centery -= self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
             self.centery += self.ai_settings.ship_speed_factor
 
-        #根据self.center更新rect对象
+        # 根据self.center更新rect对象
         self.rect.centerx = self.center
         self.rect.centery = self.centery
 
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.image,self.rect)
+        self.screen.blit(self.image, self.rect)
 
-        
-        
-        
